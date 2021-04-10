31969a895a8983b5a0e40f814746f904216c73d9
Zicen Pan <zp37@cornell.edu>
2021-04-10 01:38:41 -0400

    Match Temp

--- src/components/Professtion/Simulation/Match/ItemTypes.tsx
+++ src/components/Professtion/Simulation/Match/ItemTypes.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 export const ItemTypes = {
-    BOX: 'box',
+    A: 'a',
+    B: 'b',
+    C: 'c'
+}
+
+export const getType = (type: string) => {
+  switch (type) {
+      case 'A':
+          return ItemTypes.A
+      case 'B':
+          return ItemTypes.B
+      case 'C':
+          return ItemTypes.C
   }
-  
+  return ItemTypes.A
+}
+
+export const availTypes = [ItemTypes.A, ItemTypes.B, ItemTypes.C]
