9b6273d653bff4ff538378bb958c40a54f476db8
Your Name <32352255+woods321@users.noreply.github.com>
2021-04-27 23:23:19 +0800

    transfer ip

--- src/assets/cdnUrlConfig.tsx
+++ src/assets/cdnUrlConfig.tsx
@@ -1,9 +1,12 @@
-export const rscLogoUrl = "http://182.254.228.214/static/svg/recource-pool-logo.svg"
+const handyIp = "182.254.228.214"
+const tysIp = "111.229.91.248"
 
-export const opGuideLogoUrl = "http://182.254.228.214/static/svg/op-guide-logo.svg"
+export const rscLogoUrl = "http://"+tysIp+"/static/svg/recource-pool-logo.svg"
 
-export const lightLogoUrl = "http://182.254.228.214/static/svg/light-logo.svg"
+export const opGuideLogoUrl = "http://"+tysIp+"/static/svg/op-guide-logo.svg"
 
-export const taskDescLogoUrl = "http://182.254.228.214/static/svg/task-description-log.svg"
+export const lightLogoUrl = "http://"+tysIp+"/static/svg/light-logo.svg"
 
-export const closeBtnUrl = "http://182.254.228.214/static/svg/closing_button.svg" 
+export const taskDescLogoUrl = "http://"+tysIp+"/static/svg/task-description-log.svg"
+
+export const closeBtnUrl = "http://"+tysIp+"/static/svg/closing_button.svg" 
