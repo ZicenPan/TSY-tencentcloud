5bbb010fdcba48322d078db8c6606a324f8241f0
Zicen Pan <zp37@cornell.edu>
2021-05-04 13:29:47 -0400

    Cleanup+2.4 finalize

--- src/components/Professtion/Simulation/Form/Form.tsx
+++ src/components/Professtion/Simulation/Form/Form.tsx
@@ -14,11 +14,11 @@ interface Props {
     handleNext: () => void
 }
 
-export const Form: FC<Props> = memo(function Form({ data, handleNext}) {
+export const Form: FC<Props> = memo(function Form({ data, handleNext }) {
     const [columns, setColumns] = useState(data.columns)
     const [grid, setGrid] = useState(data.grid)
-    const [answer] = useState(data.answer)
-    const [pass, setPass] = useState(false);
+    const [answer, setAnswer] = useState(data.answer)
+    const [pass, setPass] = useState(false)
     function getType(type: string) {
         switch (type) {
             case 'select':
@@ -27,86 +27,71 @@ export const Form: FC<Props> = memo(function Form({ data, handleNext}) {
     }
 
     useEffect(() => {
-        console.log(grid)
-    }, [grid])
-
-    console.log("data", data)
-    console.log("rmsg", data.resultMsg)
-    const handleDataChange = (data) => {
-        console.log('changed', data)
-    }
-    // console.log(data.columns)
-    // const newCol = columns.map((item, index) => {
-    //     return update(item, {
-    //         editor: {
-    //             $apply: function (x) {
-    //                 return getType(x)
-    //             }
-    //         }
-    //     })
-    // })
+        setColumns(data.columns)
+        setGrid(data.grid)
+        setAnswer(data.answer)
+    }, [data])
 
     function formEqual(object1: any, object2: any) {
-        const keys1 = Object.keys(object1);
-        const keys2 = Object.keys(object2);
-      
-        if (keys1.length !== keys2.length) {
-          return false;
-        }
-      
-        for (const key of keys1) {
-          const val1 = object1[key];
-          const val2 = object2[key];
-          if (val1 !== "" && val1 !== val2) {
-            return false;
-          }
-        }
-      
-        return true;
-      }
+        const keys1 = Object.keys(object1)
+        const keys2 = Object.keys(object2)
 
-      function afterSaveCell(oldValue: any, newValue: any, row: any) {
-        console.log("grid", grid)
-        console.log("answer", answer)
-        let equal = true;
-        for (let i in answer) {
-            if (answer.hasOwnProperty(i)) {
-                equal = equal && formEqual(answer[i], grid[i]);
+        if (keys1.length !== keys2.length) {
+            return false
+        }
+
+        for (const key of keys1) {
+            const val1 = object1[key]
+            const val2 = object2[key]
+            if (val1 !== '' && val1 !== val2) {
+                return false
             }
         }
-        setPass(equal);
+
+        return true
     }
 
-    function handleCheck() {
-
+    function afterSaveCell(oldValue: any, newValue: any, row: any) {
+        console.log('grid', grid)
+        console.log('answer', answer)
+        let equal = true
+        for (let i in answer) {
+            if (answer.hasOwnProperty(i)) {
+                equal = equal && formEqual(answer[i], grid[i])
+            }
+        }
+        setPass(equal)
     }
 
-    function setShowStandardTip(){
+    function handleCheck() {}
 
-    }
+    function setShowStandardTip() {}
     if (data.columns) {
         return (
             <div className="mt-40">
                 <BootstrapTable
-                    onDataSizeChange={handleDataChange}
                     keyField="id"
                     data={grid}
                     columns={columns}
-                    cellEdit={cellEditFactory({ mode: 'click', 
-                    blurToSave: true, afterSaveCell })}
+                    cellEdit={cellEditFactory({ mode: 'click', blurToSave: true, afterSaveCell })}
                     headerClasses="header-class"
                 />
-                            <PageResult checked={pass} handleNext={handleNext} resultMsg={(data.resultMsg)?data.resultMsg:""} handleCheck={handleCheck} setShowStandardTip={setShowStandardTip}/>
+                <PageResult
+                    checked={pass}
+                    handleNext={handleNext}
+                    resultMsg={data.resultMsg ? data.resultMsg : ''}
+                    handleCheck={handleCheck}
+                    setShowStandardTip={setShowStandardTip}
+                />
 
-                            <button
-                onClick={handleNext}
-                type="submit"
-                className="btn btn-blue"
-                style={{ position: 'fixed', top: '85%', left: '90%' }}
-            >
-                Skip
-            </button>
-
+                <button
+                    onClick={handleNext}
+                    type="submit"
+                    className="btn btn-blue"
+                    style={{ position: 'fixed', top: '85%', left: '90%' }}
+                >
+                    Skip
+                </button>
             </div>
         )
     } else {
