dda23b18574a954567f9b205903f9728cce07b46
Zicen Pan <zp37@cornell.edu>
2021-05-05 11:37:47 -0400

    form 2-3

--- src/components/Professtion/Simulation/ScreenShot/ScreenShot.tsx
+++ src/components/Professtion/Simulation/ScreenShot/ScreenShot.tsx
@@ -1,4 +1,5 @@
-import React, { FC, memo, useState, useEffect } from 'react'
+/* eslint-disable max-params */
+import React, { FC, memo, useState, useEffect, useCallback } from 'react'
 
 import { Form } from '../Form/Form'
 import FakeUi from '../FakeUi/FakeUi'
@@ -18,13 +19,10 @@ interface Props {
 export const ScreenShot: FC<Props> = memo(function ScreenShot({ data, handleNext }) {
     const [UInum, setUInum] = useState(0)
 
+    const [grid, setGrid] = useState(data.form.grid)
+    const [columns, setColumns] = useState(data.form.columns)
+    const [answer, setAnswer] = useState(data.form.answer)
     const [pass, setPass] = useState(false)
-    function getType(type: string) {
-        switch (type) {
-            case 'select':
-                return Type.SELECT
-        }
-    }
 
     function formEqual(object1: any, object2: any) {
         const keys1 = Object.keys(object1)
@@ -45,18 +43,46 @@ export const ScreenShot: FC<Props> = memo(function ScreenShot({ data, handleNext
         return true
     }
 
-    // function afterSaveCell(oldValue: any, newValue: any, row: any) {
-    //     console.log('grid', grid)
-    //     console.log('answer', answer)
-    //     let equal = true
-    //     for (let i in answer) {
-    //         if (answer.hasOwnProperty(i)) {
-    //             equal = equal && formEqual(answer[i], grid[i])
-    //         }
+    function afterSaveCell(oldValue: any, newValue: any, row: any) {
+        console.log('grid', grid)
+        console.log('answer', answer)
+        let equal = true
+        for (let i in answer) {
+            if (answer.hasOwnProperty(i)) {
+                equal = equal && formEqual(answer[i], grid[i])
+            }
+        }
+        setPass(equal)
+    }
+
     //     }
     //     setPass(equal)
     // }
 
+    const newCols = update(columns, {
+        [columns.length - 1]: {
+            $merge: {
+                formatter: (cellContent, row, rowindex, extraData) => {
+                    console.log("cell", cellContent)
+                    console.log("row", row)
+                    console.log("rowi", rowindex)
+                    console.log("extra", extraData)
+                    return (
+                        <button
+                            className="btn btn-blue"
+                            onClick={() => {
+                                console.log('click')
+                            }}
+                        >
+                            截图
+                        </button>
+                    )
+                }
+            }
+        }
+    })
+
+    console.log(newCols)
     function handleCheck() {}
 
     function setShowStandardTip() {}
@@ -68,7 +94,17 @@ export const ScreenShot: FC<Props> = memo(function ScreenShot({ data, handleNext
                     <FakeUi data={data.fakeUI} />
                 </div>
                 <div className="ml-40 align-self-center">
-                    <Form data={data.form} handleNext={handleNext} />
+                    <BootstrapTable
+                        keyField="id"
+                        data={grid}
+                        columns={newCols}
+                        cellEdit={cellEditFactory({
+                            mode: 'click',
+                            blurToSave: true,
+                            afterSaveCell
+                        })}
+                        headerClasses={data.headerClass ? data.headerClass : 'header-class'}
+                    />
                 </div>
             </div>
             <button
